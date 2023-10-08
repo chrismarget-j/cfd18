d8ebe2ea1531acc6983825a63652ba5fc2fd8d4f
Chris Marget <cmarget@juniper.net>
2023-10-07 22:13:42 -0400

    add network name output

--- modules/mod_apstra_network/output.tf
+++ modules/mod_apstra_network/output.tf
@@ -7,3 +7,5 @@ output "ct_tagged" { value = apstra_datacenter_connectivity_template.tagged.id }
 output "ct_untagged" { value = apstra_datacenter_connectivity_template.untagged.id }
 
 output "ipv4_subnet" { value = apstra_datacenter_virtual_network.o.ipv4_subnet }
+
+output "name" { value = apstra_datacenter_virtual_network.o.name }
